73d100dc981576bf6f98186b24ecdbf77a17c5b1
ghemsley <ghemsley@protonmail.ch>
2020-11-07 02:39:27 -0600

    commit tree 182f559c6b93c0cb94860a6fdeb82c790b9627aa

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,65 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = new
+    song.save
+    song
+  end
+
+  def self.new_by_name(name)
+    song = new
+    song.name = name
+    song
+  end
+
+  def self.create_by_name(name)
+    song = new
+    song.name = name
+    song.save
+    song
+  end
+
+  def self.find_by_name(name)
+    all.find do |song|
+      song.name == name
+    end
+  end
+
+  def self.find_or_create_by_name(name)
+    existing_song = find_by_name(name)
+    song = if existing_song.nil?
+             create_by_name(name)
+           else
+             existing_song
+           end
+    song
+  end
+
+  def self.alphabetical
+    all.sort_by do |song|
+      song.name
+    end
+  end
+
+  def self.new_from_filename(filename)
+    song_array = filename.scan(/^([\w ]+)+ - ([\w ]+)\.mp3$/i).flatten
+    song = new
+    song.name = song_array[1]
+    song.artist_name = song_array[0]
+    song
+  end
+
+  def self.create_from_filename(filename)
+    song_array = filename.scan(/^([\w ]+)+ - ([\w ]+)\.mp3$/i).flatten
+    song = new
+    song.name = song_array[1]
+    song.artist_name = song_array[0]
+    song.save
+    song
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
 end
